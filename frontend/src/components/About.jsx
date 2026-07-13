73d8895bc8995724658f9ef2496c3b60fa883a0d
brindha257 <brindhakamaraj2006@gmail.com>
2026-07-13 12:33:52 +0530

    About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-8 text-slate-600">Career Objective</h2>
         <div className="bg-gray-80 p-8 rounded-xl shadow-md">
           <p className="text-gray-900 text-lg leading-relaxed">
-            Aspiring B.Sc. Computer Science student (3rd year) and MERN Stack Developer seeking an entry-level/fresher position 
+            B.Sc. Computer Science graduate and MERN Stack Developer seeking an entry-level/fresher position 
             where I can apply my knowledge of web development, contribute to real-world projects, and continuously enhance my 
             technical and problem-solving skills in a growth-oriented organization.
           </p>
